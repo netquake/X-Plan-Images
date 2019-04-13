9a10627b49e8327f4179fcb38104e2ac0ad84f7e
BarryPaneer <barry.paneer@qq.com>
2019-04-13 20:37:55 +0800

    add files

---
+++ clear_contains_list.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+###  Brief:   Clear the contains' list
+###  Author:  barry.paneer@qq.com
+###  Date:    2019/04/12
+
+CID_LIST=`docker ps -aq`
+
+if [ -z $CID_LIST ]; then
+	echo "Contains list : nothing to clear..."
+	return
+fi
+
+echo "clearing......"
+docker rm $CID_LIST
+echo "done!"
+
+
---
+++ clear_local_repository.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+###  Brief:   Clear all the images in local repository
+###  Author:  barry.paneer@qq.com
+###  Date:    2019/04/12
+
+IMAGE_LIST=`docker images -q`
+
+if [ -z $IMAGE_LIST ]; then
+        echo "Local repository: nothing to clear..."
+        return
+fi
+
+echo "clearing......"
+docker rmi -f $(IMAGE_LIST)
+echo "done!"
+
+
