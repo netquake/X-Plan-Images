1202206bf010bd2420c55e5281737b84363ccc19
BarryPaneer <barry.paneer@qq.com>
2019-04-14 22:33:43 +0800

    first commit

---
+++ webnode/base.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#base.sh
+
+###  Brief:   Declaration of basic info.
+###  Author:  barry.paneer@qq.com
+###  Date:    2019/04/12
+
+REPO_NAMESPACE=netquake
+REPO_IMGAE_NAME=webnode
+REPO_NAME=$REPO_NAMESPACE/$REPO_IMGAE_NAME
+REPO_TAG=v1.0.1
+LAST_IMAGE_NAME=$REPO_NAME:$REPO_TAG
+
+PYTHON_PKG_NAME=Python-3.5.7.tgz
+
+
